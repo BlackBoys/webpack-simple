1f4de16698f5ba2fbb48d6439e418db712de8e61
GoodEvent <1807657825@qq.com>
2017-04-11 18:47:27 +0800

    更改接口地址

--- app/domain.contant.js
+++ app/domain.contant.js
@@ -1,6 +1,6 @@
 angular.module('App')
     .constant('domain', {
-        zh: 'http://192.168.43.114:8080', //纵横项目
+        zh: 'http://10.0.128.36:8081', //纵横项目
         qiniuUpload: 'http://up-z1.qiniu.com', //图片上传地址
         qiniuDownload: 'http://oo6x1glmf.bkt.clouddn.com' //图片下载地址
     })
