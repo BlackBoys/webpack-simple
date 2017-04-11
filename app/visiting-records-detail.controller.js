8346655b7454995bbb6ff7da56d1f2ab508d6abb
GoodEvent <1807657825@qq.com>
2017-04-11 21:31:05 +0800

    更新截图的dom

--- app/visiting-records-detail.controller.js
+++ app/visiting-records-detail.controller.js
@@ -1,5 +1,4 @@
 import XuntongJSBridge from "XuntongJSBridge";
-import domtoimage from 'dom-to-image';
 import "./visiting-records-detail.css";
 angular.module('App').controller('VisitingRecordsDetailController', ['$http', '$scope', '$stateParams', 'domain', '$state', '$timeout', function ($http, $scope, $stateParams, domain, $state, $timeout) {
 
@@ -13,38 +12,9 @@ angular.module('App').controller('VisitingRecordsDetailController', ['$http', '$
     }
    
 
-    function imgFactory() {
-        var node = document.getElementsByTagName('html')[0];
-
-
-
-        // domtoimage.toBlob(node)
-        //     .then(function (blob) {
-        //         window.saveAs(blob, 'my-node.png');
-        //     });
-
-        // domtoimage.toPng(node)
-        //     .then(function (dataUrl) {
-        //         var img = new Image();
-        //         img.src = dataUrl;
-        //         console.log(`imgurl:${dataUrl}`);
-        //         XuntongJSBridge.call('previewImage',
-        //             {
-        //                 current: 'http://www.easyicon.net/api/resizeApi.php?id=1177605&size=128', // 当前显示图片的http链接
-        //                 urls: ['http://www.easyicon.net/api/resizeApi.php?id=1177605&size=128'] // 需要预览的图片http链接列表
-        //             }, function (result) {
-
-        //             }
-        //         );
-        //         // document.body.appendChild(img);
-        //     })
-        //     .catch(function (error) {
-        //         console.error('oops, something went wrong!', error);
-        //     });
-    }
-
     $scope.register = function (planId) {
-        $http.get(`${domain.zh}/visitor/updateCheckintime?planId=${planId}`).success(function () {
+        $http.get(`${domain.zh}/visitor/updateCheckintime?planId=${planId}`).success(function (data) {
+            alert(angular.toJson(data));
             $state.go('visiting-records');
         }).error();
     }
@@ -56,12 +26,12 @@ angular.module('App').controller('VisitingRecordsDetailController', ['$http', '$
     // $scope.showdialog();
 
     $scope.leave = function (planId) {
-        $http.get(`${domain.zh}/visitor/updateCheckouttime?planId=${planId}`).success(function () {
+        $http.get(`${domain.zh}/visitor/updateCheckouttime?planId=${planId}`).success(function (data) {
+            alert(angular.toJson(data));
             $state.go('visiting-records');
         }).error();
     }
 
-    console.dir($stateParams);
     $scope.getDetail($stateParams.planId);
     //（0：未到访；1：已到访；2：已离开；3：已作废；）
     $scope.getType = function (type) {
