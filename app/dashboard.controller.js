2e5f93904cd6a73abb62534bffb8f5af2f8ce223
GoodEvent <1807657825@qq.com>
2017-04-18 18:34:38 +0800

    规范readonly的写法

--- app/dashboard.controller.js
+++ app/dashboard.controller.js
@@ -4,13 +4,14 @@ angular.module('App').controller('DashboardController', ['$http', '$scope', '$ro
     $scope.scan = function (type) {
         XuntongJSBridge.call('scanQRCode', { 'needResult': 1, 'scanType': ['qrCode'] }, function (result) {
             // alert(angular.toJson(result));
-            $state.go('visiting-records-detail', { planId: result.data.qrcode_str, type: type });
+            if (result.success)
+                $state.go('visiting-records-detail', { planId: result.data.qrcode_str, type: type });
         });
     }
 
     let uri = new URI(window.location);
     let ticket = uri.search(true).ticket;
-    
+
 
     $scope.selectPerson = function () {
         XuntongJSBridge.call('selectPerson', { 'pType': '1' }, function (result) {
