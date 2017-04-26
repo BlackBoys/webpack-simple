f9184970e22de8129af44239ac5840c445ea7be0
GoodEvent <1807657825@qq.com>
2017-04-26 19:49:11 +0800

    正式环境配置

--- app/domain.contant.js
+++ app/domain.contant.js
@@ -1,6 +1,6 @@
 angular.module('App')
     .constant('domain', {
-        zh: 'http://10.0.128.36:8081', //纵横项目
+        zh: 'http://vrs.zemt.cn:8082', //纵横项目
         zemt: 'http://weixin.zemt.cn',
         qiniuUpload: 'http://up-z1.qiniu.com', //图片上传地址
         qiniuDownload: 'http://oo6x1glmf.bkt.clouddn.com' //图片下载地址
